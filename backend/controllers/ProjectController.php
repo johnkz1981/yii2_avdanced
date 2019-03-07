7b6bfaf4369881d4f60d8a64cb082c3555e026b7
johnkz <johnkz@inbox.ru>
2019-03-07 17:29:46 +0300

    lesson6 finish

--- backend/controllers/ProjectController.php
+++ backend/controllers/ProjectController.php
@@ -14,114 +14,125 @@ use yii\filters\VerbFilter;
  */
 class ProjectController extends Controller
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function behaviors()
-    {
-        return [
-            'verbs' => [
-                'class' => VerbFilter::className(),
-                'actions' => [
-                    'delete' => ['POST'],
-                ],
-            ],
-        ];
+  /**
+   * {@inheritdoc}
+   */
+  public function behaviors()
+  {
+    return [
+      'verbs' => [
+        'class' => VerbFilter::className(),
+        'actions' => [
+          'delete' => ['POST'],
+        ],
+      ],
+    ];
+  }
+
+  /**
+   * Lists all Project models.
+   * @return mixed
+   */
+  public function actionIndex()
+  {
+    $searchModel = new ProjectSearch();
+    $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+
+    return $this->render('index', [
+      'searchModel' => $searchModel,
+      'dataProvider' => $dataProvider,
+    ]);
+  }
+
+  /**
+   * Displays a single Project model.
+   * @param integer $id
+   * @return mixed
+   * @throws NotFoundHttpException if the model cannot be found
+   */
+  public function actionView($id)
+  {
+    return $this->render('view', [
+      'model' => $this->findModel($id),
+    ]);
+  }
+
+  /**
+   * Creates a new Project model.
+   * If creation is successful, the browser will be redirected to the 'view' page.
+   * @return mixed
+   */
+  public function actionCreate()
+  {
+    $model = new Project();
+
+    if ($model->load(Yii::$app->request->post()) && $model->save()) {
+      return $this->redirect(['view', 'id' => $model->id]);
     }
 
-    /**
-     * Lists all Project models.
-     * @return mixed
-     */
-    public function actionIndex()
-    {
-        $searchModel = new ProjectSearch();
-        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+    return $this->render('create', [
+      'model' => $model,
+    ]);
+  }
 
-        return $this->render('index', [
-            'searchModel' => $searchModel,
-            'dataProvider' => $dataProvider,
-        ]);
+  /**
+   * Updates an existing Project model.
+   * If update is successful, the browser will be redirected to the 'view' page.
+   * @param integer $id
+   * @return mixed
+   * @throws NotFoundHttpException if the model cannot be found
+   */
+  public function actionUpdate($id)
+  {
+    $model = $this->findModel($id);
+
+    if ($this->loadModel($model) && $model->save()) {
+      return $this->redirect(['view', 'id' => $model->id]);
     }
 
-    /**
-     * Displays a single Project model.
-     * @param integer $id
-     * @return mixed
-     * @throws NotFoundHttpException if the model cannot be found
-     */
-    public function actionView($id)
-    {
-        return $this->render('view', [
-            'model' => $this->findModel($id),
-        ]);
+    return $this->render('update', [
+      'model' => $model,
+    ]);
+  }
+
+  private function loadModel(Project $model)
+  {
+    $data = Yii::$app->request->post($model->formName());
+    $projectUsers = $data[Project::RELATION_PROJECT_USERS] ?? null;
+
+    if ($projectUsers !== null) {
+      $model->projectUsers = $projectUsers === '' ? [] : $projectUsers;
+    }
+    return $model->load(Yii::$app->request->post());
+  }
+
+  /**
+   * Deletes an existing Project model.
+   * If deletion is successful, the browser will be redirected to the 'index' page.
+   * @param integer $id
+   * @return mixed
+   * @throws NotFoundHttpException if the model cannot be found
+   */
+  public function actionDelete($id)
+  {
+    $this->findModel($id)->delete();
+
+    return $this->redirect(['index']);
+  }
+
+  /**
+   * Finds the Project model based on its primary key value.
+   * If the model is not found, a 404 HTTP exception will be thrown.
+   * @param integer $id
+   * @return Project the loaded model
+   * @throws NotFoundHttpException if the model cannot be found
+   */
+  protected function findModel($id)
+  {
+    if (($model = Project::findOne($id)) !== null) {
+      return $model;
     }
 
-    /**
-     * Creates a new Project model.
-     * If creation is successful, the browser will be redirected to the 'view' page.
-     * @return mixed
-     */
-    public function actionCreate()
-    {
-        $model = new Project();
-
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->id]);
-        }
-
-        return $this->render('create', [
-            'model' => $model,
-        ]);
-    }
-
-    /**
-     * Updates an existing Project model.
-     * If update is successful, the browser will be redirected to the 'view' page.
-     * @param integer $id
-     * @return mixed
-     * @throws NotFoundHttpException if the model cannot be found
-     */
-    public function actionUpdate($id)
-    {
-        $model = $this->findModel($id);
-
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->id]);
-        }
-
-        return $this->render('update', [
-            'model' => $model,
-        ]);
-    }
-
-    /**
-     * Deletes an existing Project model.
-     * If deletion is successful, the browser will be redirected to the 'index' page.
-     * @param integer $id
-     * @return mixed
-     * @throws NotFoundHttpException if the model cannot be found
-     */
-    public function actionDelete($id)
-    {
-        $this->findModel($id)->delete();
-
-        return $this->redirect(['index']);
-    }
-
-    /**
-     * Finds the Project model based on its primary key value.
-     * If the model is not found, a 404 HTTP exception will be thrown.
-     * @param integer $id
-     * @return Project the loaded model
-     * @throws NotFoundHttpException if the model cannot be found
-     */
-    protected function findModel($id)
-    {
-        if (($model = Project::findOne($id)) !== null) {
-            return $model;
-        }
-
-        throw new NotFoundHttpException('The requested page does not exist.');
-    }
+    throw new NotFoundHttpException('The requested page does not exist.');
+  }
 }
--- backend/views/project/_form.php
+++ backend/views/project/_form.php
@@ -26,45 +26,49 @@ use yii\widgets\ActiveForm;
 
   <?= $form->field($model, 'active')->dropDownList(\common\models\Project::STATUS_LABELS) ?>
 
-  <?php if (!$model->isNewRecord):
+  <?php if (!$model->isNewRecord): ?>
+
+    <?=
 
     $form->field($model, \common\models\Project::RELATION_PROJECT_USERS)->widget(\unclead\multipleinput\MultipleInput::class, [
-      // https://github.com/unclead/yii2-multiple-input
+        // https://github.com/unclead/yii2-multiple-input
 
-      'id' => 'project-users-widget',
-      'max' => 10,
-      'min' => 0,
-      'addButtonPosition' => \unclead\multipleinput\MultipleInput::POS_HEADER,
+        'id' => 'project-users-widget',
+        'max' => 10,
+        'min' => 0,
+        'addButtonPosition' => \unclead\multipleinput\MultipleInput::POS_HEADER,
 
-      /* 'columns' => [
-        'name' => 'user_id',
-        'type' => \unclead\multipleinput\MultipleInputColumn::TYPE_STATIC,
-        'value' => function ($data) {
+        'columns' => [
+          /*[
+          'name' => 'user_id',
+          'type' => \unclead\multipleinput\MultipleInputColumn::TYPE_STATIC,
+          'value' => function ($data) {
 
-          return $data ? Html::a($data->user->username, ['user/view', 'id' => $data->user]) : '';
-        }
-      ],*/
-      [
-        'name' => 'project_id',
-        'type' => 'hiddenInput',
-        'defaultValue' => $model->id
-      ],
-      [
-        'name' => 'user_id',
-        'type' => 'dropDownList',
-        'title' => 'User',
-        'items' => \common\models\User::find()->select('username')->indexBy('id')->column()
-      ],
-      [
-        'name' => 'role',
-        'type' => 'dropDownList',
-        'title' => 'Роль',
-        'items' => \common\models\ProjectUser::ROLES
+            return $data ? Html::a($data->user->username, ['user/view', 'id' => $data->user]) : '';
+          }
+        ],*/
+          [
+            'name' => 'project_id',
+            'type' => 'hiddenInput',
+            'defaultValue' => $model->id
+          ],
+          [
+            'name' => 'user_id',
+            'type' => 'dropDownList',
+            'title' => 'User',
+            'items' => \common\models\User::find()->select('username')->indexBy('id')->column()
+          ],
+          [
+            'name' => 'role',
+            'type' => 'dropDownList',
+            'title' => 'Роль',
+            'items' => \common\models\ProjectUser::ROLES
+          ]
+        ],
       ]
-    ]);
-
-  endif;
-  ?>
+    );
+    ?>
+  <?php endif; ?>
 
   <div class="form-group">
     <div class="col-md-2 col-md-offset-2">
